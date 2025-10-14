54fb5e74235ef79b81724cd3eb654016c8d4103f
DESKTOP-J0RRQOC\user1 <tr8vel7er@gmail.com>
2025-10-14 17:49:06 +0900

    2025년 10월 14일

---
+++ 12_io/src/edu/kh/pack1/run/FileRun.java
@@ -0,0 +1,15 @@
+package edu.kh.pack1.run;
+
+import edu.kh.pack1.model.service.FileService;
+
+public class FileRun {
+
+	public static void main(String[] args) {
+		FileService service = new FileService();
+		//service.method1();
+		//service.method2();
+		service.method2();
+
+	}
+
+}
